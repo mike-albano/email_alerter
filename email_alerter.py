f30f6375275c1ad9e827bf0cef583b56e0b50250
albanom@google.com <albanom@google.com>
2019-05-20 08:20:17 -0600

    Fix spacing.

--- email_alerter.py
+++ email_alerter.py
@@ -67,19 +67,19 @@ def prep_gmail():
     # Prepare gMail credentials.
     creds = None
     if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
+      with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
     # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server()
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+      if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+      else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+          'credentials.json', SCOPES)
+        creds = flow.run_local_server()
+      # Save the credentials for the next run
+      with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
 
     return build('gmail', 'v1', credentials=creds)
 
